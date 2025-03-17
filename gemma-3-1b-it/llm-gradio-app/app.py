810256bec401fc70c99b2f3b77b3a804befe5c47
Salah Uddin <salah.uddin@mail.mcgill.ca>
2025-03-17 00:32:45 -0400

    Corrected file reference

--- gemma-3-1b-it/llm-gradio-app/app.py
+++ gemma-3-1b-it/llm-gradio-app/app.py
@@ -8,7 +8,7 @@ def generate_response(user_input):
         "messages": [
             {
                 "role": "system",
-                "content": "You are a helpful assistant. Respond to each question accurately and concisely."
+                "content": "You are a helpful assistant."
             },
             {
                 "role": "user",
@@ -21,13 +21,11 @@ def generate_response(user_input):
 
     if response.status_code == 200:
         response_text = response.json()['response']
-        return response_text
-        # assistant_start = response_text.find("<|assistant|>")
-        # if assistant_start != -1:
-        #     cleaned_output = response_text[assistant_start + len("<|assistant|>"):].strip()
-        #     return cleaned_output
-        # else:
-        #     return "Could not find <|assistant|> in the response."
+        output = response_text[0]
+        cleaned_output = output.split("<start_of_turn>model")[1].strip().split("<end_of_turn>")[0].strip()
+        output_list = [line.strip() for line in cleaned_output.split("\n") if line.strip()]
+        result = "\n".join(output_list)
+        return result
     else:
         return f"Error: {response.status_code} {response.text}"
     
@@ -36,7 +34,7 @@ iface = gr.Interface(
     inputs=gr.Textbox(lines=2, placeholder="Enter your question here..."),
     outputs="text",
     title="Chatbot",
-    description="Ask me anything! "
+    description="Ask me anything!"
 )
 
 # Launch the app
