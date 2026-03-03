ee3ecade1714f20bbcecb4dd7fc8a02256342f0d
Salah Uddin <salah.uddin@mail.mcgill.ca>
2026-03-02 22:25:45 -0500

    Change pgAdmin deployment strategy

---
+++ pgadmin-config/config_local.py
@@ -0,0 +1,9 @@
+import logging
+DATA_DIR = '/var/lib/pgadmin4'
+LOG_FILE = '/var/log/pgadmin4/pgadmin4.log'
+SQLITE_PATH = '/var/lib/pgadmin4/pgadmin4.db'
+SESSION_DB_PATH = '/var/lib/pgadmin4/sessions'
+STORAGE_DIR = '/var/lib/pgadmin4/storage'
+SERVER_MODE = True
+DEFAULT_SERVER = '0.0.0.0'
+DEFAULT_SERVER_PORT = 5050
